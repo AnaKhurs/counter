88958e416cfd0fa2c8d7141bb26c33945494d5a2
AnaKhurs <ana.khurs@gmail.com>
2021-11-20 21:55:05 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,45 @@ import {Button} from "./components/Button/Button";
 function App() {
 
     let [result, setResult] = useState<number>(0)
+    let maxValue = 5;
+    let minValue = 0;
 
 
     const clickInc = () => {
-        result < 5 ? result += 1 : result = 5;
+        result < maxValue ? result++ : result = maxValue;
         setResult(result);
     }
 
     const clickReset = () => {
-        setResult(0)
+        setResult(minValue)
     }
-
 
 
     const onOffDisableInc = (result: number) => {
-        return result === 5 ? true : false
+        return result === maxValue ? true : false
     }
 
     const onOffDisableReset = (result: number) => {
-       return  result === 0 ? true : false
+        return result === minValue ? true : false
     }
-
-
 
 
     return (
         <div className="App">
-            <div className="scoreboard">
-                <Result total={result}/>
-            </div>
+            <div className="counter">
+                <div className="scoreboard">
+                    <Result total={result}/>
+                </div>
 
-            <div className="buttons">
-               {/* <IncButton clickInc={clickInc} result={result}/>
+                <div className="buttons">
+                    {/* <IncButton clickInc={clickInc} result={result}/>
                 <ResetButton clickReset={clickReset} result={result}/>*/}
-                <Button clickHandler={clickInc} onOffDisable={onOffDisableInc} result={result} name="inc" className="inc-reset"/>
-                <Button clickHandler={clickReset} onOffDisable={onOffDisableReset } result={result} name="reset" className="button-reset"/>
+                    <Button clickHandler={clickInc} onOffDisable={onOffDisableInc} result={result} name="inc"
+                            className="inc-reset"/>
+                    <Button clickHandler={clickReset} onOffDisable={onOffDisableReset} result={result} name="reset"
+                            className="button-reset"/>
+                </div>
             </div>
-
         </div>
     );
 }
